29b426f9ee786605b8c4b0dc73c7f8cd5950c7aa
Mirai <mi0mi10mi20@gmail.com>
2025-01-02 20:43:24 +0700

    Add Icons Preloading

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// Icon Preload
+const imageUrls = [
+    "/Media/pfp.png",
+    "/Media/menu.png",
+    "/Media/logos/youtube.png",
+    "/Media/logos/studio-white.png",
+    "/Media/logos/roblox.png",
+    "/Media/logos/git.png",
+    "/Media/logos/rojo.png"
+];
+
+function preloadImages(urls) {
+    urls.forEach(url => {
+        const img = new Image(); 
+        img.src = url; 
+    });
+}
+
+preloadImages(imageUrls);
+
+// Mobile Links Menu
 const mobileLinks = document.querySelector('.mobileLinks');
 
 toggleMenu = () => 
